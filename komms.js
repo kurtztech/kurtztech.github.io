d9d811157611464ef8a7dfb89bd286748cc4b1cc
kurtztech <sam@kurtztech.com>
2018-05-03 21:00:45 -0600

    new google api function

--- komms.js
+++ komms.js
@@ -7,7 +7,7 @@
 
 
 
-function start() {
+/*function start() {
   // 2. Initialize the JavaScript client library.
   gapi.client.init({
     'apiKey': 'AIzaSyDgtcf9lX8kX5Go_5ghblgZi_iN2N8U6LY',
@@ -29,4 +29,27 @@ function start() {
   });
 };
 // 1. Load the JavaScript client library.
+gapi.load('client', start);*/
+
+function start() {
+  // Initializes the client with the API key and the Translate API.
+  gapi.client.init({
+    'apiKey': 'YOUR_API_KEY',
+    'discoveryDocs': ['https://www.googleapis.com/discovery/v1/apis/translate/v2/rest'],
+  }).then(function() {
+    // Executes an API request, and returns a Promise.
+    // The method name `language.translations.list` comes from the API discovery.
+    return gapi.client.language.translations.list({
+      q: 'hello world',
+      source: 'en',
+      target: 'de',
+    });
+  }).then(function(response) {
+    console.log(response.result.data.translations[0].translatedText);
+  }, function(reason) {
+    console.log('Error: ' + reason.result.error.message);
+  });
+};
+
+// Loads the JavaScript client library and invokes `start` afterwards.
 gapi.load('client', start);
