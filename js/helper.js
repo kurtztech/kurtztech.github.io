0d16f33c3f77895246443e09c656ef97a1ec1f0e
kurtztech <sam@kurtztech.com>
2018-06-28 08:49:44 -0600

    removed ic2

--- js/helper.js
+++
@@ -1,9 +0,0 @@
-function numOut(bigNum) {
-  if (bigNum.lte(BigNumber('99'))) {
-    return bigNum.toFormat(2);
-  } else if (bigNum.lte(BigNumber('999999'))) {
-    return bigNum.toFormat(0);
-  } else {
-    return bigNum.toExponential(2);
-  }
-}
