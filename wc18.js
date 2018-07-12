67a18d7ea92c9d88b82f01f4fcdf36c61beaa742
kurtztech <sam@kurtztech.com>
2018-07-12 11:24:53 -0600

    improvements to world cup animation

--- wc18.js
+++ wc18.js
@@ -13,27 +13,34 @@ function flagTransform() {
   tl.to('.red', 1, { height: '160', y: '0', backgroundColor: '#FF0000' }, 1);
   tl.to('.title', 0.5, { text: { value: 'Vs.', newClass: 'title-vs' } }, 1);
 
-  tl.fromTo('.croat', 1.5, { y: '0' }, { y: '375', ease: Bounce.easeOut }, 1.5);
+  tl.fromTo(
+    '.croat',
+    2,
+    { y: '0', x: '0' },
+    { y: '375', ease: Back.easeOut.config(1) },
+    1
+  );
 
   tl.to('.blue', 1, { width: '639', x: '0' }, 2);
   tl.to('.white', 1, { width: '639', x: '-213' }, 2);
   tl.to('.red', 1, { width: '639', x: '-426' }, 2);
   tl.to('.title', 0.5, { text: { value: 'Croatia', newClass: 'title-cr' } }, 2);
 
-  tl.to('.blue', 1, { width: '213', x: '0' }, 3);
-  tl.to('.white', 1, { width: '213', x: '0' }, 3);
-  tl.to('.red', 1, { width: '213', x: '0' }, 3);
-  tl.to('.title', 0.5, { text: { value: 'Vs.', newClass: 'title-vs' } }, 3);
+  tl.to('.blue', 1, { width: '213', x: '0' }, 4);
+  tl.to('.white', 1, { width: '213', x: '0' }, 4);
+  tl.to('.red', 1, { width: '213', x: '0' }, 4);
+  tl.to('.title', 0.5, { text: { value: 'Vs.', newClass: 'title-vs' } }, 4);
 
-  tl.to('.blue', 1, { height: '480', y: '0', backgroundColor: '#00267f' }, 4);
-  tl.to('.white', 1, { height: '480', y: '0' }, 4);
-  tl.to('.red', 1, { height: '480', y: '0', backgroundColor: '#f31830' }, 4);
-  tl.to('.croat', 0.75, { ease: Back.easeIn.config(1.5), y: '800' }, 4);
-  tl.to('.title', 0.5, { text: { value: 'France', newClass: 'title-fr' } }, 4);
+  tl.to('.croat', 0.75, { ease: Back.easeIn.config(3), x: '450' }, 4);
+
+  tl.to('.blue', 1, { height: '480', y: '0', backgroundColor: '#00267f' }, 5);
+  tl.to('.white', 1, { height: '480', y: '0' }, 5);
+  tl.to('.red', 1, { height: '480', y: '0', backgroundColor: '#f31830' }, 5);
+  tl.to('.title', 0.5, { text: { value: 'France', newClass: 'title-fr' } }, 5);
 }
 
 flagTransform();
 
 setInterval(function() {
   flagTransform();
-}, 4500);
+}, 5500);
